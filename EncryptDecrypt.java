d877c391afb8afa0923580387fb9f64ea5b524f9
CigdemErs <36510610+CigdemErs@users.noreply.github.com>
2018-07-15 21:27:37 +0000

    Delete EncryptDecrypt.java

--- EncryptDecrypt.java
+++
@@ -1,6 +0,0 @@
-package odev;
-
-public interface EncryptDecrypt {
-	public void encrypt(String inputFileName, String key);
-	public void decrypt(String inputFileName, String key, String outputFileName);
-}
